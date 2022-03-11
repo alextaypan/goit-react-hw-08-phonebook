9ef3d531dadeff313957fe116404e4381d7764cc
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-03-11 14:21:04 +0200

    docs: add home img

--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Thumb, Title, SecondTitle } from "./HomeView.styled.jsx";
+import defaultPhonebook from "./phonebook.jpg";
 
 const HomeView = () => (
   <Thumb>
     <Title>Welcome to Phonebook App!</Title>
     <SecondTitle>
-      Please, <b>Register</b> or <b>Log in</b> to have access to your Contacts!
+      Please, <b>Sign up</b> or <b>Log in</b> to have access to your Contacts!
     </SecondTitle>
+    <img src={defaultPhonebook} alt="Книга контактов" />
   </Thumb>
 );
 
