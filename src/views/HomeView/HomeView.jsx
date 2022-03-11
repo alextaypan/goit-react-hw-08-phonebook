b0d1794103e8c92c7d5069969974d989a223e725
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-03-11 14:50:32 +0200

    docs: toggle homeview reg or logIn

--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import authSelectors from "../../redux/auth/auth-selectors";
 import { Thumb, Title, SecondTitle } from "./HomeView.styled.jsx";
-import defaultPhonebook from "./phonebook.jpg";
+import defaultPhonebookImg from "./phonebook.jpg";
 
-const HomeView = () => (
-  <Thumb>
-    <Title>Welcome to Phonebook App!</Title>
-    <SecondTitle>
-      Please, <b>Sign up</b> or <b>Log in</b> to have access to your Contacts!
-    </SecondTitle>
-    <img src={defaultPhonebook} alt="Книга контактов" />
-  </Thumb>
-);
-
-export default HomeView;
+export default function HomeView() {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  return (
+    <Thumb>
+      <Title>Welcome to Phonebook App!</Title>
+      {!isLoggedIn && (
+        <SecondTitle>
+          Please, <b>Sign up</b> or <b>Log in</b> to have access to your
+          Contacts!
+        </SecondTitle>
+      )}
+      <img src={defaultPhonebookImg} alt="Книга контактов" />
+    </Thumb>
+  );
+}
